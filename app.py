668b127d6964beaad6326d243eb542d6537ef5f8
Crys7a1Grain <safronov1706@mail.ru>
2024-06-27 02:46:57 +0300

    added item icons

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import messagebox
 import requests
+import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from PIL import Image, ImageTk
+import io
+
+image_refs = []
 
 
 def fetch_data_from_api(url):
@@ -13,9 +19,14 @@ def fetch_data_from_api(url):
         return None
 
 
-def paginate_data(items, page_size):
-    pages = [items[i:i + page_size] for i in range(0, len(items), page_size)]
-    return pages
+def fetch_image_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Проверяем успешность запроса
+        return Image.open(io.BytesIO(response.content))
+    except requests.exceptions.RequestException as e:
+        messagebox.showerror("Error", f"Error fetching image: {e}")
+        return None
 
 
 def display_page(replica_items, text_area, page_num, total_pages):
@@ -28,10 +39,22 @@ def display_page(replica_items, text_area, page_num, total_pages):
     if items_to_display:
         text_area.insert(tk.END, f"Page {page_num + 1} of {total_pages}\n\n")
         for item in items_to_display:
-            text_area.insert(tk.END, f"Item: {item['name']}\n")
-            text_area.insert(tk.END, f"  Chaos Value: {item['chaosValue']}\n")
-            text_area.insert(
-                tk.END, f"  Divine Value: {item['divineValue']}\n\n")
+            icon_url = item['icon']
+            icon_image = fetch_image_from_url(icon_url)
+            if icon_image:
+                icon_image.thumbnail((50, 50))
+                icon = ImageTk.PhotoImage(icon_image)
+                text_area.image_create(tk.END, image=icon)
+                text_area.insert(tk.END, "\n")  # Новый ряд для текста
+                text_area.insert(tk.END, f"Item: {item['name']}\n")
+                text_area.insert(
+                    tk.END, f"  Chaos Value: {item['chaosValue']}\n")
+                text_area.insert(
+                    tk.END, f"  Divine Value: {item['divineValue']}\n\n")
+                text_area.image_ref = icon  # Ссылка на изображение, чтобы не собиралось GC
+                image_refs.append(icon)
+            else:
+                text_area.insert(tk.END, "Item wasn't loaded.")
     else:
         text_area.insert(tk.END, "No Replica items found.")
 
@@ -79,8 +102,10 @@ def update_page_buttons(replica_items, page_var, text_area, total_pages_var, pag
 
     if total_pages > 1:
         for i in range(total_pages):
-            button = ttk.Button(page_buttons_frame, text=f"{i + 1}", command=lambda i=i: go_to_page(
-                i, page_var, text_area, replica_items, total_pages_var))
+            button = tb.Button(page_buttons_frame, text=f"{i + 1}",
+                               command=lambda i=i: go_to_page(
+                                   i, page_var, text_area, replica_items, total_pages_var),
+                               bootstyle="info-outline")
             button.grid(row=0, column=i, padx=2)
 
 
@@ -88,13 +113,17 @@ def update_nav_buttons(page_var, total_pages_var, nav_buttons_frame, text_area):
     for widget in nav_buttons_frame.winfo_children():
         widget.destroy()
 
-    prev_button = ttk.Button(nav_buttons_frame, text="Previous", command=lambda: go_to_page(
-        max(page_var.get() - 1, 0), page_var, text_area, replica_items, total_pages_var))
-    prev_button.grid(row=0, column=0, pady=10, sticky=(tk.W, tk.E))
+    prev_button = tb.Button(nav_buttons_frame, text="Previous",
+                            command=lambda: go_to_page(
+                                max(page_var.get() - 1, 0), page_var, text_area, replica_items, total_pages_var),
+                            bootstyle="primary")
+    prev_button.grid(row=0, column=0, padx=5)
 
-    next_button = ttk.Button(nav_buttons_frame, text="Next", command=lambda: go_to_page(
-        min(page_var.get() + 1, total_pages_var.get() - 1), page_var, text_area, replica_items, total_pages_var))
-    next_button.grid(row=0, column=1, pady=10, sticky=(tk.W, tk.E))
+    next_button = tb.Button(nav_buttons_frame, text="Next",
+                            command=lambda: go_to_page(min(page_var.get(
+                            ) + 1, total_pages_var.get() - 1), page_var, text_area, replica_items, total_pages_var),
+                            bootstyle="primary")
+    next_button.grid(row=0, column=1, padx=5)
 
 
 def on_fetch_click(text_area, page_size, page_var, total_pages_var, page_buttons_frame, nav_buttons_frame):
@@ -113,8 +142,8 @@ def go_to_page(page_num, page_var, text_area, replica_items, total_pages_var):
 
 
 def draw_window():
-    # Создание главного окна
-    root = tk.Tk()
+    # Создание главного окна с использованием ttkbootstrap
+    root = tb.Window(themename="superhero")  # Используем тему 'darkly'
     root.title("POE Ninja Replica Fetcher")
     root.geometry("800x600")
 
@@ -123,35 +152,38 @@ def draw_window():
     total_pages_var = tk.IntVar(value=1)
 
     # Создание фрейма ввода
-    frame = ttk.Frame(root, padding="10")
+    frame = tb.Frame(root, padding="10")
     frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
     frame.columnconfigure(0, weight=1)
     frame.rowconfigure(0, weight=1)
 
     # Создание текстовой области для отображения данных
-    text_area = tk.Text(frame, wrap=tk.WORD, width=80, height=20)
+    text_area = tb.Text(frame, wrap=tk.WORD, width=90,
+                        height=25, font=("Arial", 10))
     text_area.grid(row=1, column=0, columnspan=3,
                    sticky=(tk.W, tk.E, tk.N, tk.S))
 
     # Создание прокрутки
-    scrollbar = ttk.Scrollbar(
-        frame, orient=tk.VERTICAL, command=text_area.yview)
+    scrollbar = tb.Scrollbar(frame, orient=tk.VERTICAL,
+                             command=text_area.yview)
     scrollbar.grid(row=1, column=3, sticky=(tk.N, tk.S))
 
     # Привязка прокрутки к текстовой области
     text_area.config(yscrollcommand=scrollbar.set)
 
     # Добавление кнопки
-    fetch_button = ttk.Button(frame, text="Fetch Data",
-                              command=lambda: on_fetch_click(text_area, page_size, page_var, total_pages_var, page_buttons_frame, nav_buttons_frame))
+    fetch_button = tb.Button(frame, text="Fetch Data",
+                             command=lambda: on_fetch_click(
+                                 text_area, page_size, page_var, total_pages_var, page_buttons_frame, nav_buttons_frame),
+                             bootstyle="success")
     fetch_button.grid(row=0, column=0, pady=10)
 
     # Создание фрейма для кнопок страниц
-    page_buttons_frame = ttk.Frame(frame)
+    page_buttons_frame = tb.Frame(frame)
     page_buttons_frame.grid(row=2, column=2, pady=10, sticky=(tk.W, tk.E))
 
     # Создание фрейма для навигационных кнопок
-    nav_buttons_frame = ttk.Frame(frame)
+    nav_buttons_frame = tb.Frame(frame)
     nav_buttons_frame.grid(row=2, column=0, pady=10,
                            columnspan=2, sticky=(tk.W, tk.E))
 
